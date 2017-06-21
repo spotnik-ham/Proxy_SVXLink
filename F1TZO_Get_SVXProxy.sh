144b64f4d25cda2bf7906400460646f41ef43d76
f5nlg <f5nlg@users.noreply.github.com>
2017-06-22 01:31:58 +0200

    Modified version of GetProxy for run in spotnik
    gawk in place of awk
    /etc/spotnik in place of /etc/svxlink

--- F1TZO_Get_SVXProxy.sh
+++ F1TZO_Get_SVXProxy.sh
@@ -24,16 +24,17 @@
 #	The selection is made on a single test so as not to take too long
 #	The result is given by BPROXY variables (such as BestProxy)
 #	With the purely informative BLAT variable (such as Best Latency)
+#	put gawk  in place of awk for compatibility with DEBIAN 8 / ARMBIAN ( F5NLG )
 
 LAT=9999
 BLAT=9999
 
-lynx -dump http://www.echolink.org/proxylist.jsp | grep Ready | grep 8100 | awk -F '[[:space:]][[:space:]]+' '{ print $3 }' | grep -v ":" | grep -v "192." | grep -v "44."> /tmp/List-Free.txt
+lynx -dump http://www.echolink.org/proxylist.jsp | grep Ready | grep 8100 | gawk -F '[[:space:]][[:space:]]+' '{ print $3 }' | grep -v ":" | grep -v "192." | grep -v "44."> /tmp/List-Free.txt
 NBP=`wc ./List-Free.txt | awk '{ print $1 }'`
 
 while read PROXY
 do
-	LAT=`ping -q -c 1 -W 1 $PROXY | grep rtt | awk  -F "/" '{ print $6 }'`
+	LAT=`ping -q -c 1 -W 1 $PROXY | grep rtt | gawk  -F "/" '{ print $6 }'`
 	echo "PROXY="$PROXY" LAT="$LAT
 	LAT=${LAT%.*}
 	LAT="${LAT:-9999}"
@@ -64,13 +65,13 @@ do
                 LIGNE="PROXY_PASSWORD=PUBLIC"
         fi
         echo $LIGNE >> /tmp/new.conf
-done < /etc/svxlink/svxlink.d/ModuleEchoLink.conf
+done < /etc/spotnik/svxlink.d/ModuleEchoLink.conf
 
 #
 # On remplace le fichler ModuleEchoLink.conf par le nouveau 
 # We replace the old ModuleEchoLink.conf by the new one
 #
-cp /tmp/new.conf /etc/svxlink/svxlink.d/ModuleEchoLink.conf
+cp /tmp/new.conf /etc/spotnik/svxlink.d/ModuleEchoLink.conf
 #
 # On redemarre SVXlink dans un Screen 
 # We restart SVXlink in a Screen 
